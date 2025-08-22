5165a7aba3f8b0e70165ec5a8f3e174946b1e88e
Isaque Pereira dos Santos <isaquevidaltube@gmail.com>
2025-08-22 17:12:44 -0300

    add task solution after code review 4

--- src/App.tsx
+++ src/App.tsx
@@ -28,10 +28,10 @@ export class App extends Component<{}, State> {
       <div className="App">
         <p className="App__message">
           {pressedKey
-            ? `The last pressed key is ${pressedKey}`
+            ? `The last pressed key is [${pressedKey}]`
             : 'Nothing was pressed yet'}
         </p>
       </div>
     );
-  };
+  }
 }
